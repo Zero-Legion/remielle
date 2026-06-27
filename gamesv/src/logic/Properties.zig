Zero-Legion/remielle

commit 67dab9c5f4bf9ec23abefbc90264a3597a2c5bb9

gamesv: fix skin misc data

gamesv/src/logic/Properties.zig
@@ -438,12 +438,14 @@ pub const BasicInfo = struct {
     avatar: HallAvatar,
     control_avatar: HallAvatar,
     control_guise_avatar: HallAvatar.Guise,
+    control_guise_avatar_skin: HallAvatar.Guise.Skin,
 
     pub const init: BasicInfo = .{
         .level = .max,
         .avatar = .wise,
         .control_avatar = .wise,
         .control_guise_avatar = .fromIdUnchecked(.remielle),
+        .control_guise_avatar_skin = .none,
     };
 };
 
@@ -483,6 +485,8 @@ pub const HallAvatar = enum(u32) {
         belle = @intFromEnum(HallAvatar.belle),
         _,
 
+        pub const Skin = Properties.Avatar.Skin;
+
         pub const FromRawIdError = error{
             InvalidAvatarId,
             AvatarNotUnlocked,
@@ -501,6 +505,13 @@ pub const HallAvatar = enum(u32) {
                 else
                     error.AvatarNotUnlocked,
             };
+        }
+
+        pub fn getSkin(guise: Guise, player_avatar_prop: *const Avatar) Skin {
+            if (guise == .none) return .none;
+
+            const index = player_avatar_prop.indexes.get(@enumFromInt(guise.toInt())).?;
+            return player_avatar_prop.meta[index].skin;
         }
 
         pub fn fromIdUnchecked(id: templates.avatar_base.Id) Guise {
@@ -522,6 +533,7 @@ pub fn toPlayerSave(props: *Properties.List, arena: Allocator, player: Player) A
         .avatar_id = basic_info.avatar.toInt(),
         .control_avatar_id = basic_info.control_avatar.toInt(),
         .control_guise_avatar_id = basic_info.control_guise_avatar.toInt(),
+        .control_guise_avatar_skin_id = basic_info.control_guise_avatar_skin.toInt(),
     };
 
     const avatar = props.getPtr(.avatar, index);
@@ -673,6 +685,7 @@ pub fn fromPlayerSave(
         .avatar = @enumFromInt(basic.avatar_id),
         .control_avatar = @enumFromInt(basic.control_avatar_id),
         .control_guise_avatar = @enumFromInt(basic.control_guise_avatar_id),
+        .control_guise_avatar_skin = @enumFromInt(basic.control_guise_avatar_skin_id),
     } else .init;
 
     if (save.avatar) |avatar_save| {

gamesv/src/logic/Changes.zig
@@ -174,7 +174,10 @@ pub const NpcInteraction = struct {
 
 pub const ControlAvatar = Properties.HallAvatar;
 
-pub const ControlGuiseAvatar = Properties.HallAvatar.Guise;
+pub const ControlGuiseAvatar = struct {
+    guise: Properties.HallAvatar.Guise,
+    guise_skin: Properties.HallAvatar.Guise.Skin,
+};
 
 /// Avatar modification.
 pub const Avatar = struct {

gamesv/src/logic/mutators/player.zig
@@ -10,8 +10,10 @@ pub fn mutateBasicInfo(
     if (changes.control_avatar) |control_avatar|
         properties.basic_info.control_avatar = control_avatar;
 
-    if (changes.control_guise_avatar) |control_guise_avatar|
-        properties.basic_info.control_guise_avatar = control_guise_avatar;
+    if (changes.control_guise_avatar) |control_guise_avatar| {
+        properties.basic_info.control_guise_avatar = control_guise_avatar.guise;
+        properties.basic_info.control_guise_avatar_skin = control_guise_avatar.guise_skin;
+    }
 }
 
 const logic = @import("../../logic.zig");

gamesv/src/messaging/handlers/avatar.zig
@@ -72,9 +72,11 @@ pub fn avatarSkinDress(
     message: Message(pb.AvatarSkinDressCsReq),
     properties: Properties.Immutable(.{
         Properties.Avatar,
+        Properties.BasicInfo,
     }),
     changes: Changes.Builder(.{
         Changes.Avatar,
+        Changes.ControlGuiseAvatar,
     }),
     response: Response(pb.AvatarSkinDressScRsp),
 ) !void {
@@ -111,6 +113,12 @@ pub fn avatarSkinDress(
         };
 
         changes.insert(avatars);
+
+        if (properties.basic_info.control_guise_avatar.toInt() == @intFromEnum(properties.avatar.ids[index]))
+            changes.insert(Changes.ControlGuiseAvatar{
+                .guise = properties.basic_info.control_guise_avatar,
+                .guise_skin = @enumFromInt(message.data.avatar_skin_id),
+            });
     }
 
     response.set(.init);
@@ -120,9 +128,11 @@ pub fn avatarSkinUnDress(
     message: Message(pb.AvatarSkinUnDressCsReq),
     properties: Properties.Immutable(.{
         Properties.Avatar,
+        Properties.BasicInfo,
     }),
     changes: Changes.Builder(.{
         Changes.Avatar,
+        Changes.ControlGuiseAvatar,
     }),
     response: Response(pb.AvatarSkinUnDressScRsp),
 ) !void {
@@ -152,6 +162,12 @@ pub fn avatarSkinUnDress(
         };
 
         changes.insert(avatars);
+
+        if (properties.basic_info.control_guise_avatar.toInt() == @intFromEnum(properties.avatar.ids[index]))
+            changes.insert(Changes.ControlGuiseAvatar{
+                .guise = properties.basic_info.control_guise_avatar,
+                .guise_skin = .none,
+            });
     }
 
     response.set(.init);

gamesv/src/messaging/handlers/misc.zig
@@ -59,6 +59,7 @@ pub fn getMiscData(
         .business_card = .init,
         .player_accessory = .{
             .control_guise_avatar_id = properties.basic_info.control_guise_avatar.toInt(),
+            .control_guise_avatar_skin_id = properties.basic_info.control_guise_avatar_skin.toInt(),
         },
     } });
 }

gamesv/src/messaging/handlers/player.zig
@@ -40,7 +40,7 @@ pub fn modAvatar(
     const new_control_avatar = Properties.HallAvatar.fromInt(message.data.control_avatar_id) orelse
         return response.fail(1);
 
-    const new_control_guise_avatar = Properties.HallAvatar.Guise.fromRawId(
+    const new_guise = Properties.HallAvatar.Guise.fromRawId(
         properties.avatar,
         message.data.control_guise_avatar_id,
     ) catch |err| return switch (err) {
@@ -52,8 +52,11 @@ pub fn modAvatar(
     if (properties.basic_info.control_avatar != new_control_avatar)
         changes.insert(new_control_avatar);
 
-    if (properties.basic_info.control_guise_avatar != new_control_guise_avatar)
-        changes.insert(new_control_guise_avatar);
+    if (properties.basic_info.control_guise_avatar != new_guise)
+        changes.insert(Changes.ControlGuiseAvatar{
+            .guise = new_guise,
+            .guise_skin = new_guise.getSkin(properties.avatar),
+        });
 
     response.set(.init);
 }

gamesv/src/messaging/notifiers/player_sync.zig
@@ -68,7 +68,10 @@ fn buildPlayerAccessory(
     const control_guise_avatar = maybe_control_guise_avatar orelse
         return null;
 
-    return .{ .control_guise_avatar_id = control_guise_avatar.toInt() };
+    return .{
+        .control_guise_avatar_id = control_guise_avatar.guise.toInt(),
+        .control_guise_avatar_skin_id = control_guise_avatar.guise_skin.toInt(),
+    };
 }
 
 fn buildItemSync(allocator: Allocator, avatar_changes: []const logic.Changes.Avatar) !?pb.ItemSync {

rmpb/src/pb.main.desc.zig
@@ -4512,7 +4512,7 @@ pub const PlayerAccessoryData = struct {
     pub const EMFNPNHCLMB_field_desc: FieldDesc = .{ .number = 10, .xor = 13528 };
     pub const LHBNHAJLKEJ_field_desc: FieldDesc = .{ .number = 11, .xor = 0 };
     pub const OHKHGBNFJME_field_desc: FieldDesc = .{ .number = 13, .xor = 0 };
-    pub const OKJBEKMDBPD_field_desc: FieldDesc = .{ .number = 15, .xor = 15941 };
+    pub const control_guise_avatar_skin_id_field_desc: FieldDesc = .{ .number = 15, .xor = 15941 };
 };
 
 pub const ONLBAFALIAD = struct {
@@ -28837,7 +28837,7 @@ pub const PlayerAccessorySync = struct {
     pub const EMFNPNHCLMB_field_desc: FieldDesc = .{ .number = 6, .xor = 13238 };
     pub const CCPCIIPLFDG_field_desc: FieldDesc = .{ .number = 7, .xor = 0 };
     pub const OHKHGBNFJME_field_desc: FieldDesc = .{ .number = 11, .xor = 0 };
-    pub const OKJBEKMDBPD_field_desc: FieldDesc = .{ .number = 15, .xor = 4302 };
+    pub const control_guise_avatar_skin_id_field_desc: FieldDesc = .{ .number = 15, .xor = 4302 };
 };
 
 pub const MHELCCCHFCI = struct {

commit 7ffd31c9521588ec17ee28e7bdd5d4e363d1cb28

Properties.HallAvatar.Guise: `wise` and `belle` are not regular avatars
Fixes regression introduced by 67dab9c5f4

gamesv/src/logic/Properties.zig
@@ -501,10 +501,13 @@ pub const HallAvatar = enum(u32) {
         }
 
         pub fn getSkin(guise: Guise, player_avatar_prop: *const Avatar) Skin {
-            if (guise == .none) return .none;
-
-            const index = player_avatar_prop.indexes.get(@enumFromInt(guise.toInt())).?;
-            return player_avatar_prop.meta[index].skin;
+            return switch (guise) {
+                .none, .wise, .belle => .none,
+                _ => |guise_avatar_id| guise_avatar_id: {
+                    const index = player_avatar_prop.indexes.get(@enumFromInt(guise_avatar_id.toInt())).?;
+                    break :guise_avatar_id player_avatar_prop.meta[index].skin;
+                },
+            };
         }
 
         pub fn fromIdUnchecked(id: templates.avatar_base.Id) Guise {